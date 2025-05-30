165f77598d4a72aa1e2c3f52b69d00583d2f5fbd
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2025-05-29 23:20:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,34 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
+// In-memory message storage
+let messages = [];
+
+function cleanupOldMessages() {
+  const threeHoursAgo = Date.now() - 3 * 60 * 60 * 1000;
+  messages = messages.filter(msg => msg.timestamp > threeHoursAgo);
+}
+
 app.use(express.static('public'));
 
 io.on('connection', socket => {
   console.log('A user connected');
-  
+
+  cleanupOldMessages();
+
+  // Send only recent messages
+  const recentMessages = messages.map(m => m.text);
+  recentMessages.forEach(msg => {
+    socket.emit('chat message', msg);
+  });
+
   socket.on('chat message', msg => {
-    io.emit('chat message', msg); // Broadcast message to everyone
+    const entry = {
+      text: msg,
+      timestamp: Date.now()
+    };
+    messages.push(entry);
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
